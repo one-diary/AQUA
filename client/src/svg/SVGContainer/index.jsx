437666c964f95c6ba21554ba20e074cc3acca7cb
Aman Jagdev <amankumarjagdev@gmail.com>
2021-04-07 11:18:23 +0530

    Added assets required

---
+++ client/src/svg/SVGContainer/index.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const SVGContainer = styled.div`
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	margin: 0 1rem 0 0;
+`
+export default SVGContainer;
