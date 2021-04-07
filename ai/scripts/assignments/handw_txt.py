c4377d3c45610eb70f231f3f0f04eeecc27100ba
radioactive11 <roy.arijit2001@gmail.com>
2021-04-07 10:14:50 +0530

    integrated data pipeline with ocr:building_construction:

---
+++ ai/scripts/assignments/handw_txt.py
@@ -0,0 +1,33 @@
+import os
+import time
+from . import azure_blob_utils
+from .plag_detector import run_script
+from . import ocr
+
+
+def convert_txt(subject: str, tag: str):
+    URLS = azure_blob_utils.get_file_url(subject, tag)
+    for file in URLS:
+        filename = file.split('/')[-1]
+        loc_id = ocr.get_operation_id(file)
+                
+        time.sleep(5)    
+        
+        content = ocr.run_ocr(loc_id)
+
+        ocr.save_file(filename, content=content)
+    
+    
+def run_plag_detection():
+    results = run_script()
+    dump_files = os.listdir("DUMP")
+    
+    for file in dump_files:
+        if file != '.gitignore':
+                os.remove(os.path.join("DUMP", file))
+                
+    return results
+    
+
+# convert_txt("coa", "Probability")
+# run_plag_detection()
---
+++ ai/scripts/assignments/ocr.py
@@ -0,0 +1,54 @@
+import time
+import http.client
+import requests
+import secret
+import os
+
+def get_operation_id(file_url: str):
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': secret.API_KEY,
+    }
+
+    URL = "{endpoint}vision/v3.1/read/analyze".format(endpoint=secret.ENDPOINT)
+
+    body_schema = {
+        "url": file_url
+    }
+    res = requests.post(URL, headers=headers, json=body_schema)
+    loc_id = res.headers['Operation-Location'].split('/')[-1]
+
+    return loc_id
+
+
+def run_ocr(location_id: str):
+    URL = "https://ocr-alpr.cognitiveservices.azure.com/vision/v3.1/read/analyzeResults/{loc_id}".format(loc_id=location_id)
+    headers={
+        "Ocp-Apim-Subscription-Key": secret.API_KEY
+    }
+    s = ""
+    res = requests.get(URL, headers=headers).json()
+    
+    
+    for item in res['analyzeResult']['readResults']:
+        for w in item['lines']:
+            for t in w['words']:
+                s += str(t['text']) + ' '
+
+    return s
+
+def save_file(filename: str, content: str):
+    filename = filename[:-4]
+    filename += '.txt'
+    DIR_PATH = os.path.join(os.getcwd(), "DUMP")
+    FILE_PATH = os.path.join(DIR_PATH, filename)
+    with open(FILE_PATH, 'a') as afile:
+        afile.write(content)
+
+     
+    
+# loc_id = get_operation_id("https://aquabucket.blob.core.windows.net/coa/assignments/Probability/answer/bme.pdf")
+# time.sleep(10)
+# cont = run_ocr(loc_id)
+# save_file("arijit", cont)
