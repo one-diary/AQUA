8e99e736ec41e8ec0d5c6d8e175cbdea781f9da5
radioactive11 <roy.arijit2001@gmail.com>
2021-04-08 10:33:11 +0530

    added file handling endpoints

--- ai/app.py
+++ ai/app.py
@@ -41,5 +41,56 @@ class QuizGenerator(Resource):
 api.add_resource(QuizGenerator, '/quiz')
 
 
+class UploadQuestion(Resource):
+    def post(self):
+        app.logger.info("Upload Question hit") # log
+        app.logger.info(request.get_json()) # log
+        print(request.form) # log
+        subject = request.form.get('subject')
+        doc = request.files['doc']
+        fname = doc.filename
+        
+        DIR = os.path.join(os.getcwd(), "DUMP")
+        LOCAL_PATH = os.path.join(DIR, fname)
+        ASSIGNMENT_NAME = fname.split('.')[0]
+        REMOTE_FILE_DIR = 'assignments/{dir_name}/question/'.format(dir_name=ASSIGNMENT_NAME) + fname
+        doc.save(LOCAL_PATH)
+        doc_url = upload_file(subject, LOCAL_PATH, REMOTE_FILE_DIR)
+        
+        files = os.listdir(os.path.join(os.getcwd(), "DUMP"))
+        for file in files:
+            if file != '.gitignore':
+                os.remove(os.path.join("DUMP", file))
+        
+        return jsonify({"url": doc_url})
+    
+api.add_resource(UploadQuestion, '/assignment/upload/question')
+
+
+class UploadAnswer(Resource):
+    def post(self):
+        subject = request.form.get('subject')
+        tag = request.form.get('tag')
+        doc = request.files['doc']
+        fname = doc.filename
+        
+        LOCAL_FILE_DIR = os.path.join("DUMP", fname)
+        REMOTE_FILE_DIR = 'assignments/{atag}/answer/'.format(atag=tag) + fname
+        
+        doc.save(LOCAL_FILE_DIR)
+        doc_url = upload_file(subject, LOCAL_FILE_DIR, REMOTE_FILE_DIR)
+        
+        files = os.listdir(os.path.join(os.getcwd(), "DUMP"))
+        for file in files:
+            if file != '.gitignore':
+                os.remove(os.path.join("DUMP", file))
+        
+
+        return jsonify({"url": doc_url})
+
+api.add_resource(UploadAnswer, '/assignment/upload/answer')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
